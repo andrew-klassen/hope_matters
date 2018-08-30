9449e2d20d3c1168d1f89d0967946f54617d215d
root <root@test>
2018-08-29 20:51:13 -0700

    fix carrage return

--- source_code/update_files/update_1.08/update.sh
+++ source_code/update_files/update_1.08/update.sh
@@ -55,6 +55,7 @@ if [ "$response" = "y" ]; then
 	echo '' >> /var/www/html/php/database_credentials.php
 	echo "***** update 1.08 *****" >> /var/www/html/php/database_credentials.php
 	echo "\$password_hashing_algorithim = PASSWORD_BCRYPT;" >> /var/www/html/php/database_credentials.php
+	sed -i 's/^M//g' /var/www/html/php/database_credentials.php
 
 	echo "*** Update Successful ***"
 	
